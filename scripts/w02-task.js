459e421213311180e1848a36dfca57b45733699d
EskolaHJ <esk23003@byui.edu>
2023-09-23 20:03:15 -0600

    Allot of changes still havent isnt working

---
+++ scripts/w02-task.js
@@ -0,0 +1,41 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Jacob Eskola"
+let currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const profilePicture = 'images/ProfilePicture.png';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector("img[src='images/ProfilePicture.png']");
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textcontent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', 'Profile image of ${fullName}')
+
+
+
+
+
+/* Step 5 - Array */
+const favoriteFood = ['Rice', 'Chicken', 'Cream of Chicken'];
+const foodString = favoriteFood.join(",");
+foodElement.innerHTML = foodString;
+const moreFavoriteFood = "Crab";
+favoriteFood.push(moreFavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+
+
