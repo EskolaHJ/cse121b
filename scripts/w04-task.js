78ccf4b5951227538a57ce4c65f5234e11442bd3
EskolaHJ <esk23003@byui.edu>
2023-10-06 07:51:30 -0600

    added some code for now.

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,11 +1,36 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-
+let myProfile = {
+    name: "Jacob Eskola",
+    photo:'image/ProfilePicture.png',
+    favoriteFood: [
+        "Rice",
+        "Chicken",
+        "Cream of Chicken",
+    ],
+    hobbies: [
+        "Going to the gym",
+        "Gaming",
+        "Scripture Study",
+        "Attempting to MOD Games"
+    ],
+    placesLived: []
+}
 
 
 
 /* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: 'Cascade, ID',
+        length: '2 years',
+        place: 'Garden City, ID',
+        length: '18 years',
+        place: 'Boise, ID',
+        length: '8 years'
+    }
+)
 
 
 
@@ -13,6 +38,7 @@
 /* DOM Manipulation - Output */
 
 /* Name */
+document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
 
