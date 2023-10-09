a970e608bd9ee8a182f79063ad50e1805cad192e
EskolaHJ <esk23003@byui.edu>
2023-10-09 14:25:37 -0600

    So much to learn :'(

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -3,8 +3,8 @@
 /* Profile Object  */
 let myProfile = {
     name: "Jacob Eskola",
-    photo:'image/ProfilePicture.png',
-    favoriteFood: [
+    photo:`images/ProfilePicture.png`,
+    favoriteFoods: [
         "Rice",
         "Chicken",
         "Cream of Chicken",
@@ -22,15 +22,10 @@ let myProfile = {
 
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
-    {
-        place: 'Cascade, ID',
-        length: '2 years',
-        place: 'Garden City, ID',
-        length: '18 years',
-        place: 'Boise, ID',
-        length: '8 years'
-    }
-)
+  {place: 'Cascade, ID', length: '2 years'},
+  {place: 'Garden City, Id', length: '18 years'},
+  {place: 'Boise, ID', length: '8 years'}
+);
 
 
 
@@ -41,14 +36,34 @@ myProfile.placesLived.push(
 document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
-
+let photoElem = document.querySelector('#photo');
+photoElem.setAttribute('src', myProfile.photo);
+photoElem.setAttribute('alt', myProfile.name);
 
 /* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food =>{
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+});
 
 
 /* Hobbies List */
+myProfile.hobbies.forEach(hobby =>{
+   let li = document.createElement('li');
+   li.textContent = hobby;
+   document.querySelector('#hobbies').appendChild(li);
+});
 
 
 /* Places Lived DataList */
+myProfile.placesLived.forEach(placeLived => {
+    let dt = document.createElement('dt');
+    dt.textContent = placeLived.place;
 
+    let dd = document.createElement('dd');
+    dd.textContent = placeLived.length;
 
+    document.querySelector('#places-lived').appendChild(dt);
+    document.querySelector('#places-lived').appendChild(dd);
+});
